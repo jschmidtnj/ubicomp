28870cd89f496abee7a408da0ea9b5d6b1cc9080
Joshua <jschmidtnj@gmail.com>
2022-02-10 18:25:30 -0500

    ubicomp: finish a1
    Signed-off-by: Joshua <jschmidtnj@gmail.com>

--- running_buddy/embedded/test.cpp
+++ running_buddy/embedded/test.cpp
@@ -1,6 +1,5 @@
 #include <Arduino.h>
 #include <Arduino_MKRIoTCarrier.h>
-#include <sstream>
 
 #include "pitches.h"
 #include "carriers.h"
@@ -202,14 +201,6 @@ void touchpage()
   }
 }
 
-std::ostringstream ss;
-
-void clear_ss()
-{
-  ss.str("");
-  ss.clear();
-}
-
 // Check the Env, IMU and light sensors
 void sensorsPage()
 {
@@ -289,41 +280,6 @@ void sensorsPage()
     {
       shakeCheckCompleted = true;
     }
-  }
-
-  for (;;)
-  {
-    Serial.println("loop");
-    float a_x, a_y, a_z, g_x, g_y, g_z;
-    while (!carrier.IMUmodule.accelerationAvailable())
-    {
-      Serial.println("wait accel");
-      delay(100);
-    }
-    if (carrier.IMUmodule.accelerationAvailable())
-    {
-      carrier.IMUmodule.readAcceleration(a_x, a_y, a_z);
-    }
-    while (!carrier.IMUmodule.gyroscopeAvailable())
-    {
-      Serial.println("wait gyro");
-      delay(100);
-    }
-    if (carrier.IMUmodule.gyroscopeAvailable())
-    {
-      carrier.IMUmodule.readGyroscope(g_x, g_y, g_z);
-    }
-    carrier.display.fillScreen(0x0000);
-    carrier.display.setCursor(25, 60);
-    carrier.display.setTextSize(1);
-    ss << "accel: " << a_x << ", " << a_y << ", " << a_z;
-    carrier.display.print(ss.str().c_str());
-    Serial.println(ss.str().c_str());
-    clear_ss();
-    carrier.display.setCursor(60, 180);
-    ss << "gyro: " << g_x << ", " << g_y << ", " << g_z;
-    carrier.display.print(ss.str().c_str());
-    delay(1000);
   }
 
   // provide a feedback on the display when movement is successfully detected
